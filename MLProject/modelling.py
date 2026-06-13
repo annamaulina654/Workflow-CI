d3778c0e541d0f1dc96b7d0c9086cf1e8e966744
annamaulina654 <annamaulina14@gmail.com>
2026-06-13 17:22:34 +0700

    Initial MLProject

---
+++ MLProject/modelling.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("telco_churn_processed.csv")
+
+X = df.drop("Churn_Yes", axis=1)
+y = df["Churn_Yes"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+mlflow.autolog()
+
+with mlflow.start_run():
+
+    model = LogisticRegression(
+        max_iter=1000
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(
+        y_test,
+        y_pred
+    )
+
+    print("Accuracy:", accuracy)
