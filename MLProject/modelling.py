309ea5fe01dbaa1ce515bc935a34e52ef4d2ea35
annamaulina654 <annamaulina14@gmail.com>
2026-06-14 00:38:56 +0700

    fix: workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -28,49 +28,40 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 mlflow.autolog()
 
-with mlflow.start_run():
+model = LogisticRegression(max_iter=1000)
 
-    model = LogisticRegression(
-        max_iter=1000
-    )
+model.fit(X_train, y_train)
 
-    model.fit(X_train, y_train)
+joblib.dump(model, "model.pkl")
 
-    joblib.dump(
-        model,
-        "model.pkl"
-    )
+y_pred = model.predict(X_test)
 
-    y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+# log metric manual
+mlflow.log_metric("accuracy", accuracy)
+mlflow.log_metric("precision", precision)
+mlflow.log_metric("recall", recall)
+mlflow.log_metric("f1_score", f1)
 
-    # log metric manual
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.log_metric("precision", precision)
-    mlflow.log_metric("recall", recall)
-    mlflow.log_metric("f1_score", f1)
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model"
+)
 
-    # simpan model
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        name="model"
-    )
+metrics = {
+    "accuracy": float(accuracy),
+    "precision": float(precision),
+    "recall": float(recall),
+    "f1_score": float(f1)
+}
 
-    # artifact tambahan
-    metrics = {
-        "accuracy": float(accuracy),
-        "precision": float(precision),
-        "recall": float(recall),
-        "f1_score": float(f1)
-    }
+with open("metrics.json", "w") as f:
+    json.dump(metrics, f, indent=4)
 
-    with open("metrics.json", "w") as f:
-        json.dump(metrics, f, indent=4)
-
-    mlflow.log_artifact("metrics.json")
+mlflow.log_artifact("metrics.json")
 
 print("Accuracy:", accuracy)
