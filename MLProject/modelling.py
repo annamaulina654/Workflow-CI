22a9dd17f9b9c820c2c9cf1749f0eace76e9ef8a
annamaulina654 <annamaulina14@gmail.com>
2026-06-13 17:54:09 +0700

    add artifact upload

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import json
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score
+)
 
+# Load Dataset
 df = pd.read_csv("telco_churn_processed.csv")
 
 X = df.drop("Churn_Yes", axis=1)
@@ -30,9 +37,34 @@ with mlflow.start_run():
 
     y_pred = model.predict(X_test)
 
-    accuracy = accuracy_score(
-        y_test,
-        y_pred
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    # log metric manual
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
+
+    # simpan model
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        name="model"
     )
 
-    print("Accuracy:", accuracy)
+    # artifact tambahan
+    metrics = {
+        "accuracy": float(accuracy),
+        "precision": float(precision),
+        "recall": float(recall),
+        "f1_score": float(f1)
+    }
+
+    with open("metrics.json", "w") as f:
+        json.dump(metrics, f, indent=4)
+
+    mlflow.log_artifact("metrics.json")
+
+print("Accuracy:", accuracy)
